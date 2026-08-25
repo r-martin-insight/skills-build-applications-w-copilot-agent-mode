4c8aaeee688b97556fe65b1b3dd8223bafd43431
r-martin-insight <86116031+r-martin-insight@users.noreply.github.com>
2026-08-25 17:38:11 +0000

    again

--- octofit-tracker/backend/src/config/apiUrl.ts
+++
@@ -1,6 +0,0 @@
-const codespaceName = process.env.CODESPACE_NAME;
-export const apiPort = 8000;
-
-export const apiBaseUrl = codespaceName
-  ? `https://${codespaceName}-${apiPort}.app.github.dev`
-  : `http://localhost:${apiPort}`;
